7409fd1b77a332c4457afb4a3c6dce8c2c56eea3
Kaveera725 <199208697+Kaveera725@users.noreply.github.com>
2026-05-03 14:52:47 +0530

    create dockerfile edit app.py and setup requirment.txt

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, flask!'
+    return "<h1>Hello, Flask on Cloud Run!</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=False)
